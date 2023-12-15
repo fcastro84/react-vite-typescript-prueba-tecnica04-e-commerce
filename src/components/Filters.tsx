15b4684af76c96b26e723d4cc92a85fd63152db5
Fidel <fenrique.castro@gmail.com>
2023-12-14 23:05:20 -0500

    useId on Filters

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,8 +1,11 @@
-import { ChangeEvent} from 'react'
+import { ChangeEvent, useId} from 'react'
 import './Filters.css'
 import useFilters from '../hooks/useFilters'
 
 const Filters = () => {
+
+    const inputRangeId = useId()
+    const selectId = useId()
 
     const { filter, setFilter } = useFilters()
     
@@ -17,14 +20,14 @@ const Filters = () => {
   return (
     <section className="filters"> 
         <div>
-          <label htmlFor="">Price:</label>
-         <input type="range" name="price"  id="" min={0} max={2000} onChange={handleChangeFilter} value={filter.price} />
+          <label htmlFor={inputRangeId}>Price:</label>
+         <input type="range" name="price"  id={inputRangeId} min={0} max={2000} onChange={handleChangeFilter} value={filter.price} />
          <span>${filter.price}</span>
         </div>
 
         <div>
-          <label htmlFor="">Category:</label>
-          <select name="category" id="" onChange={handleChangeFilter}>
+          <label htmlFor={selectId}>Category:</label>
+          <select name="category" id={selectId} onChange={handleChangeFilter}>
             <option value="All">All</option>
             <option value="skincare">Skincare</option>
             <option value="groceries">Groceries</option>
